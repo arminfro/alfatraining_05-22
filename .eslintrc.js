8d61310321d9e1c8152f5b83db4639c59bf3d378
Armin Fröhlich <mail@arminfroehlich.de>
2020-11-18 09:05:16 +0100

    init react

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react-perf/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react", "react-perf", "@typescript-eslint"],
+  rules: {
+    // https://github.com/typescript-eslint/typescript-eslint/issues/363
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-unused-vars": "off",
+    "no-debugger": "warn", // instead of default error
+    indent: ["warn", 2, { SwitchCase: 1 }], // 2 instead of default 4 indention
+  },
+};
