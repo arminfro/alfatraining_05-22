86e874c904a18b7171c251e1d8afb3238c0f62d8
Armin Fröhlich <mail@arminfroehlich.de>
2021-01-12 09:03:26 +0100

    A6, optional, add Routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,14 @@
 import { ReactElement } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import BookList from "./BookList";
-import BookDetails from "./BookDetails";
-import Home from "./Home";
 import Layout from "./Layout";
+import Routes from "./Routes";
 
 export default function App(): ReactElement {
   return (
     <BrowserRouter>
       <Layout>
-        <Routes>
-          <Route path="/books/:isbn" element={<BookDetails />} />
-          <Route path="/books" element={<BookList />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Navigate to="/home" />} />
-        </Routes>
+        <Routes />
       </Layout>
     </BrowserRouter>
   );
