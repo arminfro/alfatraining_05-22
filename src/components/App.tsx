098e5d97b82170b4add1719817bfc1cf73e5d3c5
Armin Fröhlich <mail@arminfroehlich.de>
2021-01-12 08:20:00 +0100

    add bulma css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,12 @@ import React, { ReactElement } from "react";
 
 export default function App(): ReactElement {
   return (
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload.
-    </p>
+    <div className="container">
+      <section className="hero is-info is-fullheight">
+        <div className="hero-body">
+          <p className="title">My Title</p>
+        </div>
+      </section>
+    </div>
   );
 }
